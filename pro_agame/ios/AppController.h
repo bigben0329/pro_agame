3a6b43ccb070c8fd6a7a92e97ec4e814a25692f4
benliu <ben@vip.qq.com>
2013-09-02 00:51:28 +0800

    Initial Commit

---
+++ pro_agame/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  pro_agameAppController.h
+//  pro_agame
+//
+//  Created by momo@ben on 13-9-2.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ pro_agame/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  pro_agameAppController.h
+//  pro_agame
+//
+//  Created by momo@ben on 13-9-2.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
